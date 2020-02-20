6c4309295a77366ed24d5137939586e9790d4f09
jianghan0712 <jianghan0712@126.com>
2020-02-20 17:13:28 +0800

    增加ignite pom

--- fams-service/src/main/java/com/purefun/fams/service/ApplicationInit.java
+++ fams-service/src/main/java/com/purefun/fams/service/ApplicationInit.java
@@ -1,5 +1,11 @@
 package com.purefun.fams.service;
 
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteLogger;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,22 +34,38 @@ public class ApplicationInit implements ApplicationRunner {
 
 	@Autowired
 	private KafkaConsumer consumer;
+	
+	@Autowired
+	private IgniteConfiguration config;
+	
+	@Autowired
+	private CacheConfiguration cacheCfg;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		logger.info("serviceName:{}", util.getBean(FAMSProducer.class));
+		
+		logger.info("config:{}", config);
 
-		TestBO_OTW bo = (TestBO_OTW) BoFactory.createBo(TestBO.class);
-		bo.setUsername("jianghan");
-		bo.setAge("20");
+		Ignite ignite = Ignition.start(config);
+		IgniteCache<String, String> cache = ignite.getOrCreateCache(cacheCfg);
+		cache.put("1", "jianghan");
+		cache.put("2", "cuiqing");
+		
+		logger.info("1:{}", cache.get("1"));
+		
 
-		producer.publish(bo);
-
-		TestBO2_OTW bo2 = (TestBO2_OTW) BoFactory.createBo(TestBO2.class);
-		bo2.setWorkid("zhangjunqing");
-		bo2.setHomeaddress("31231");
-
-		producer.publish(bo2);
+//		TestBO_OTW bo = (TestBO_OTW) BoFactory.createBo(TestBO.class);
+//		bo.setUsername("jianghan");
+//		bo.setAge("20");
+//
+//		producer.publish(bo);
+//
+//		TestBO2_OTW bo2 = (TestBO2_OTW) BoFactory.createBo(TestBO2.class);
+//		bo2.setWorkid("zhangjunqing");
+//		bo2.setHomeaddress("31231");
+//
+//		producer.publish(bo2);
 	}
 
 }
